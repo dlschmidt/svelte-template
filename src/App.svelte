522c1eabd67603696069f0ed89b8c4d9cb98176f
Dennis Schmidt <20640295+dlschmidt@users.noreply.github.com>
2022-02-22 04:31:52 +0100

    Added tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,13 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
+	<div class="container mx-auto">
+		<div class="text-lg font-bold">Hello {name}!</div>
+	</div>
 </main>
 
-<style>
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
